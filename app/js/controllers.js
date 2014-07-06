3c5577fd604370d74379673ed557c4af796b344e
addisonbean <addisonbean@gmail.com>
2014-07-05 19:48:49 -0500

    Search works

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,13 @@ app.controller('fruitOrNotController', ['$scope', '$http',
 		$http.defaults.transformResponse = [];
 
 		$http.get('json/foods.json').success(function(data) {
-			lists = JSON.parse(data)
+			lists = JSON.parse(data);
 			$scope.fruitList = lists['fruit'];
 			$scope.vegetableList = lists['vegetable'];
 			$scope.allFood = $scope.fruitList.concat($scope.vegetableList)
+			$scope.allFood = $scope.allFood.filter(function(elem, pos) {
+				return $scope.allFood.indexOf(elem) == pos;
+			})
 		});
 
 		$scope.queryIsEmpty = function() {
